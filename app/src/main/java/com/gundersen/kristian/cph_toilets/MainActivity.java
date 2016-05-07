19ba0dcf7f344dc834871b86e85cf90fb8516aac
kristianhg <kristian.h.gundersen@gmail.com>
2016-05-07 14:49:48 +0200

    Added location!!!

--- app/src/main/java/com/gundersen/kristian/cph_toilets/MainActivity.java
+++ app/src/main/java/com/gundersen/kristian/cph_toilets/MainActivity.java
@@ -2,18 +2,21 @@ package com.gundersen.kristian.cph_toilets;
 
 
 
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.Context;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -24,13 +27,23 @@ import com.google.maps.android.geojson.GeoJsonLayer;
 
 import org.json.JSONObject;
 
-import java.net.URL;
-import java.util.logging.Logger;
-
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements GoogleMap.OnMyLocationButtonClickListener, OnMapReadyCallback,
+        ActivityCompat.OnRequestPermissionsResultCallback {
 
     private GoogleMap uiGoogleMap;
     private JSONObject savedJson;
+
+    /**
+     * Request code for location permission request.
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in
+     * {@link #onRequestPermissionsResult(int, String[], int[])}.
+     */
+    private boolean mPermissionDenied = false;
 
     /// SET UP REFERENCES
     @Override
@@ -43,10 +56,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         SupportMapFragment supportMapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.googleMap));
         supportMapFragment.getMapAsync(this);
+
+
     }
 
     public void onSaveInstanceState(Bundle savedInstanceState) {
-        savedInstanceState.putString("Json",savedJson.toString());
+        savedInstanceState.putString("Json", savedJson.toString());
         super.onSaveInstanceState(savedInstanceState);
     }
 
@@ -57,6 +72,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             savedJson = new JSONObject(savedString);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+        if(savedJson != null) {
+
         }
     }
 
@@ -80,53 +98,61 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     private void runInfo() {
-        Logger.getAnonymousLogger().info("--> runinfo()");
         Intent intent = new Intent(this,AppInfo.class);
         startActivity(intent);
     }
 
 
-
     //CONFIGURING MAP
     public void onMapReady (GoogleMap googleMap) {
-        Logger.getAnonymousLogger().info("MAP READY");
         uiGoogleMap = googleMap;
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        googleMap.getMinZoomLevel();
+        uiGoogleMap.setOnMyLocationButtonClickListener(this);
+        enableMyLocation();
+
+        uiGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        uiGoogleMap.getMinZoomLevel();
+        uiGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+
         ////// NEXT LINE IS A PLACEHOLDER. It should be generated from the user´s position. the last arg is zoom level.
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(55.6815890, 12.5290920), 12.0f));
 
 
-        Logger.getAnonymousLogger().info("MAP SEEKING JSON");
         if (savedJson != null) {
-            Logger.getAnonymousLogger().info("JSON FILE ALREADY EXISTS");
             GeoJsonLayer layer = new GeoJsonLayer(uiGoogleMap, savedJson);
             layer.addLayerToMap();
-            Logger.getAnonymousLogger().info("MAP GOT EXISTING JSON");
         }
         else {
-            Logger.getAnonymousLogger().info("DOWNLOADING JSON");
             new AddJsonToMapAsync().execute();
         }
+    }
 
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (uiGoogleMap != null) {
+            // Access to the location has been granted to the app.
+            uiGoogleMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
     }
 
 
-// ASYNC TASK TO FETCH JSON AND PUT IT ON THE MAP
+
+    // ASYNC TASK TO FETCH JSON AND PUT IT ON THE MAP
     public class AddJsonToMapAsync extends AsyncTask<Void, String, JSONObject> {
-
-        /*// PROGRESS SPINNER, IGNORE UNTIL IMPORTANT STUFF WORKS
-        private ProgressDialog spinner;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            // Showing progress dialog
-            spinner.setMessage("Please wait...");
-            spinner.setCancelable(true);
-            spinner.show();
-        }
-        //*/
 
         @Override
         protected JSONObject doInBackground(Void... Args) {
@@ -137,13 +163,50 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         @Override
         protected void onPostExecute(JSONObject geoJson) {
             savedJson = geoJson;
-            GeoJsonLayer layer = new GeoJsonLayer(uiGoogleMap, geoJson);
-            layer.addLayerToMap();
-            Logger.getAnonymousLogger().info("MAP GOT DOWNLOADED JSON");
-
+            if (uiGoogleMap != null) {
+                GeoJsonLayer layer = new GeoJsonLayer(uiGoogleMap, geoJson);
+                layer.addLayerToMap();
+            }
 
         }
 
+    }
+
+    //Getting permissions
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
 
 }
